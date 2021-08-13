59275def5e0c7daa389e7dab047e576a90556786
Sufyan-Kamran <76629748+Sufyan-Kamran@users.noreply.github.com>
2021-08-13 09:48:37 -0700

    Add files via upload

--- experiment.py
+++ experiment.py
@@ -1,2 +1,42 @@
-Import tkinter as tk
-import module
+from tkinter import filedialog
+import mysql.connector
+from mysql.connector import Error
+import pymysql
+
+def ctb(filename):
+    with open(filename,'rb') as file:
+        binarydata=file.read()
+    return binarydata
+def convertBTF(binarydata,filename):
+    with open(filename,'wb')as file:
+        file.write(binarydata)
+
+host = "localhost"
+user = "root"
+password = ""
+database = "example"
+
+
+def openfile():
+    global filepath
+    filepath = filedialog.askopenfilename()
+    
+    file= open(filepath, 'rb').read()
+openfile()
+print(filepath)
+
+
+
+try:
+    convertPic = ctb(filepath)
+    
+    con = pymysql.connect(host="localhost", user="root", password="", database="example" )
+    cur = con.cursor()
+    
+    cur.execute("insert into imgs (hello) value(%s)",(convertPic))
+    con.commit()
+    con.close()    
+    
+    
+except Exception as e:
+    print(e)
